31a43d58f44836ce532fec1467528e0c90c87fed
160250013-hue <160250013@ggi.ac.in>
2026-05-14 14:52:55 +0530

    fix ssl postgres

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,9 +7,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: false
 });
 
 export const query = (text, params) => pool.query(text, params);
